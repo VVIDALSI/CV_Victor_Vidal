44dcdc9d621634c2b9eca239eb9488231e9c13f3
VVIDALSI <106885575+VVIDALSI@users.noreply.github.com>
2023-10-13 18:49:39 -0500

    Correccion de apertura en youtube

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import imgSnK from "../../Images/Logo White.png"
 import stlPortfolio from "./Portfolio.module.css"
 
 
 export default function Portfolio() {
     const openVideoInNewTab = () => {
-        const videoUrl = "https://vimeo.com/874230497?share=copy"; // Cambia esta URL por la del video que desees abrir
+        const videoUrl = "https://www.youtube.com/watch?v=skCbK21GJIA&t=4s";
         window.open(videoUrl, "_blank");
     };
 
@@ -13,11 +13,13 @@ export default function Portfolio() {
     return (
         <div className={stlPortfolio.contPortfolio}>
 
-            <div className={stlPortfolio.linkProject} onClick={openModal}>
+
+            <div className={stlPortfolio.linkProject} onClick={openVideoInNewTab}>
                 <span>Strings & Keys</span>
                 <img src={imgSnK} alt="SnK" />
                 <span>01/ago/2023 - 08/sep/2023</span>
             </div>
+
 
         </div>
     );
